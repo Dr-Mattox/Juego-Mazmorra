dc978868d06300ac3030aaff3adce2656943ecd8
Alex Jose De Mattos Perez <demattosalex@gmail.com>
2024-10-07 23:40:45 -0500

    Update Dragon.java

---
+++ Dragon.java
@@ -0,0 +1,10 @@
+package juegoMazmorra;
+
+public class Dragon extends Enemigo { // Hereda de enemigo
+
+	// Constructor
+	public Dragon() {
+		super(70, 20); // 70 de salud, 25 de daño // al darle super estamos asignandole los atributos
+						// de su clase padre
+	}
+}
