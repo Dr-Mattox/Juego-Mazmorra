0f621ba391bbcb49b89c4178860b10a252671b34
Alex Jose De Mattos Perez <demattosalex@gmail.com>
2024-10-07 23:43:19 -0500

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package juegoMazmorra;
+
+public class Main { // esta clase solo la uso para correr el juego ya que desde el menu se empieza
+					// el juego dependiendo que se quiere hacer
+	public static void main(String[] args) {
+		MenuPrincipal menu = new MenuPrincipal();
+		menu.mostrarMenu();
+	}
+}
