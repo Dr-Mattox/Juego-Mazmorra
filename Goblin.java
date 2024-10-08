524ea0e5c61ea7bc490910e6a4e5b41b51b167f5
Alex Jose De Mattos Perez <demattosalex@gmail.com>
2024-10-07 23:41:52 -0500

    Update Goblin.java

---
+++ Goblin.java
@@ -0,0 +1,10 @@
+package juegoMazmorra;
+
+public class Goblin extends Enemigo { // Hereda de enemigo
+
+	// Constructor
+	public Goblin() {
+		super(50, 15); // 50 de salud, 15 de daño // al darle super estamos asignandole los atributos
+						// de su clase padre
+	}
+}
