7e3c6052a4d9b7fced6c839084552bb20f2ce6d2
Alex Jose De Mattos Perez <demattosalex@gmail.com>
2024-10-07 23:47:21 -0500

    Update PocionRecuperacion.java

---
+++ PocionRecuperacion.java
@@ -0,0 +1,9 @@
+package juegoMazmorra;
+
+public class PocionRecuperacion extends Objeto { // Hereda de objetos
+
+	// Constructor
+	public PocionRecuperacion() {
+		this.nombre = "Poción de Recuperación";
+	}
+}
